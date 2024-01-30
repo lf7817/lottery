733f0d4fb78d24bf6355bb422a53194ed2b7bd7a
lf7817 <535536456@qq.com>
2024-01-30 16:34:44 +0800

    feat(server): 集成ServeStaticModule， 完善 build 命令

--- packages/app-server/src/app.module.ts
+++ packages/app-server/src/app.module.ts
@@ -1,6 +1,8 @@
+import { join } from 'node:path'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CacheModule } from '@nestjs/cache-manager'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import configs from './configs/config'
 import { SignInModule } from './modules/sign-in/sign-in.module'
 import { WxModule } from './modules/wx/wx.module'
@@ -14,6 +16,20 @@ import { WxModule } from './modules/wx/wx.module'
       envFilePath: ['.env.local'],
     }),
     CacheModule.register({ ttl: 5000, isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../app-sign/dist'),
+      serveRoot: '/sign',
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../app-screen/dist'),
+      serveRoot: '/screen',
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
     WxModule,
     SignInModule,
   ],
