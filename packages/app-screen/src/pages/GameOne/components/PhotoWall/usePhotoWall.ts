80f5ae8d7bf156997fb79a858e16c09d5cbe3ec7
lf7817 <535536456@qq.com>
2024-02-03 22:48:58 +0800

    feat(screen): 实现showWinners逻辑， 解决世界坐标转局部坐标问题

--- packages/app-screen/src/pages/GameOne/components/PhotoWall/usePhotoWall.ts
+++ packages/app-screen/src/pages/GameOne/components/PhotoWall/usePhotoWall.ts
@@ -55,24 +55,55 @@ export default function usePhotoWall() {
    * 展示获奖人员
    * @param winners
    */
-  const showWinners = (winners: Object3D[]) => {
-    const targetPosition = new Vector3(0, 0, 10)
-    const groupPosition = new Vector3(0, 0, 14)
+  const showWinners = async (winners: Object3D[]) => {
+    // 要保证cards的 retation.y 为 2n * Math.PI才行
+    const parentPosition = new Vector3(0, 0, 14)
 
-    winners.forEach((winner) => {
-      const localPosition = targetPosition.add(groupPosition)
+    const cardWidth = 6
+    const cardHeight = 8
 
-      gsap.to(winner.position, {
-        x: localPosition.x,
-        y: localPosition.y,
-        z: localPosition.z,
-        duration: 1,
-        ease: 'power1.inOut',
-        onComplete: () => {
-          winner.lookAt(0, 0, 14)
-        },
-      })
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        let count = 0
+        winners.forEach((winner, index) => {
+          const maxPerRow = Math.min(5, winners.length)
+          // winners长度最大为 10，每行最多渲染 5 个，超过换行，卡片宽 6 高 8， 帮我计算下 x y
+          const x = (index % maxPerRow) * cardWidth - (maxPerRow - 1) * cardWidth / 2 + cardWidth / 4
+          const y = Math.floor(index / maxPerRow) * cardHeight - (Math.floor((winners.length - 1) / maxPerRow)) * cardHeight / 2
+
+          const targetPosition = new Vector3(x, y, 10)
+          const localPosition = targetPosition.add(parentPosition)
+
+          gsap.to(winner.position, {
+            x: localPosition.x,
+            y: localPosition.y,
+            z: localPosition.z,
+            duration: 1,
+            ease: 'power1.inOut',
+            // @ts-expect-error
+            onComplete: () => ++count === winners.length && resolve(true),
+          })
+
+          gsap.to(winner.rotation, {
+            x: 0,
+            y: 0,
+            z: 0,
+            duration: 1,
+            ease: 'power1.inOut',
+          })
+
+          gsap.to(winner.scale, {
+            x: 2,
+            y: 2,
+            z: 2,
+            duration: 1,
+            ease: 'power1.inOut',
+          })
+        })
+      }, 2300)
     })
+
+    await celebrateFireworks()
   }
 
   /**
@@ -81,7 +112,7 @@ export default function usePhotoWall() {
   const draw = (start: boolean) => {
     if (start) {
       gameOneAction.draw(true)
-      transformObjects(cards.current!.children, data.sphere)
+      transformObjects(cards.current!.children, data.sphere, { duration: 1, delay: 0 })
     } else {
       const winners = gameOneAction.draw(false)
 
@@ -92,7 +123,6 @@ export default function usePhotoWall() {
           .filter(Boolean)
 
         showWinners(cardObjects)
-        celebrateFireworks()
       }
     }
   }
@@ -120,7 +150,7 @@ export default function usePhotoWall() {
         elapsedTime.current = 0
       }
       else {
-        cards.current.rotation.y += deltaY * 0.02
+        cards.current.rotation.y += Math.min(elapsedTime.current * 0.02, deltaY * 0.03)
       }
     }
   })
--- packages/app-screen/src/utils/index.ts
+++ packages/app-screen/src/utils/index.ts
@@ -6,19 +6,25 @@ export function getAssetPath(filePath: string) {
   return `${import.meta.env.BASE_URL}${filePath}`
 }
 
-export function transformObjects(objects: Object3D[], targets: Object3D[]) {
+interface TransformObjectsOptions {
+  duration?: number
+  delay?: number
+}
+
+export function transformObjects(objects: Object3D[], targets: Object3D[], options?: TransformObjectsOptions) {
+  const { duration = 2, delay = 2 } = options || {}
   return Promise.all(objects.map((object) => {
     return new Promise((resolve) => {
       let count = 0
       // eslint-disable-next-line style/max-statements-per-line
-      const onComplete = () => { ++count === 2 && resolve(true) }
+      const onComplete = () => { ++count === 3 && resolve(true) }
       const target = targets.find(item => item.name === object.name)!
       gsap.to(object.position, {
         x: target.position.x,
         y: target.position.y,
         z: target.position.z,
-        duration: Math.random() * 2 + 1,
-        delay: Math.random() * 2,
+        duration: Math.random() * duration + 1,
+        delay: Math.random() * delay,
         ease: 'power1.inOut',
         onComplete,
       })
@@ -27,8 +33,18 @@ export function transformObjects(objects: Object3D[], targets: Object3D[]) {
         x: target.rotation.x,
         y: target.rotation.y,
         z: target.rotation.z,
-        duration: Math.random() * 2 + 1,
-        delay: Math.random() * 2,
+        duration: Math.random() * duration + 1,
+        delay: Math.random() * delay,
+        ease: 'power1.inOut',
+        onComplete,
+      })
+
+      gsap.to(object.scale, {
+        x: target.scale.x,
+        y: target.scale.y,
+        z: target.scale.z,
+        duration: Math.random() * duration + 1,
+        delay: Math.random() * delay,
         ease: 'power1.inOut',
         onComplete,
       })
