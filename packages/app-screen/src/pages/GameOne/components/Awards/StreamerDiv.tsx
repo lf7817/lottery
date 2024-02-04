5d7fbe605caab76debc99ec7446ca116e5a7a8a4
buran123 <15195860740@163.com>
2024-02-04 14:42:57 +0800

    feat:  流光div设计

---
+++ packages/app-screen/src/pages/GameOne/components/Awards/StreamerDiv.tsx
@@ -0,0 +1,90 @@
+import * as stylex from '@stylexjs/stylex'
+import {FC, ReactElement} from "react";
+
+interface Props{
+  active:boolean
+  content:ReactElement
+}
+const animate1 = stylex.keyframes({
+  '0%': {left: '-100%'},
+  '50%': {left: '100%'},
+  '100%': {left: '100%'},
+})
+const animate2 = stylex.keyframes({
+  '0%': {top: '-100%'},
+  '50%': {top: '100%'},
+  '100%': {top: '100%'},
+})
+const animate3 = stylex.keyframes({
+  '0%': {right: '-100%'},
+  '50%': {right: '100%'},
+  '100%': {right: '100%'},
+})
+const animate4 = stylex.keyframes({
+  '0%': {bottom: '-100%'},
+  '50%': {bottom: '100%'},
+  '100%': {bottom: '100%'},
+})
+export const styles= stylex.create({
+  spinner:(active)=>( {
+    transform: active ? 'scale(1.2)' : 'scale(1)',
+    transformOrigin: 'left center',
+    transition: 'all 0.5s ease-in-out',
+    position: 'relative',
+    overflow: 'hidden',
+    margin: active ? '30px 0' : '20px 0',
+  }),
+  spin:{
+    'position':'absolute',
+    'display':'block',
+    ':nth-child(1)':{
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '2px',
+      background: 'linear-gradient(90deg,transparent,#03e9f4)',
+      animation: `${animate1} 1s linear infinite`,
+    },
+    ':nth-child(2)':{
+      top: '-100%',
+      right: 0,
+      width: '2px',
+      height: '100%',
+      background: 'linear-gradient(180deg,transparent,#03e9f4)',
+      animation: `${animate2} 1s linear infinite`,
+      animationDelay: '0.25s'
+    },
+    ':nth-child(3)':{
+      bottom: 0,
+      right: 0,
+      width: '100%',
+      height: '2px',
+      background: 'linear-gradient(270deg,transparent,#03e9f4)',
+      animation: `${animate3} 1s linear infinite`,
+      animationDelay: '0.50s'
+    },
+    ':nth-child(4)':{
+      bottom: '-100%',
+      left: 0,
+      width: '2px',
+      height: '100%',
+      background: 'linear-gradient(360deg,transparent,#03e9f4)',
+      animation: `${animate4} 1s linear infinite`,
+      animationDelay: '0.75s'
+    }
+  }
+
+})
+
+export const StreamerDiv:FC<Props>=({active,content})=> {
+  return (
+    <div {...stylex.props(styles.spinner(active))}>
+      {active&&<>
+        <span {...stylex.props(styles.spin)}></span>
+        <span {...stylex.props(styles.spin)}></span>
+        <span {...stylex.props(styles.spin)}></span>
+        <span {...stylex.props(styles.spin)}></span></>}
+      {content}
+    </div>
+  )
+}
--- packages/app-screen/src/pages/GameOne/components/Awards/index.tsx
+++ packages/app-screen/src/pages/GameOne/components/Awards/index.tsx
@@ -3,6 +3,7 @@ import * as stylex from '@stylexjs/stylex'
 import { useSnapshot } from 'valtio'
 import styles from '@/pages/GameOne/components/Awards/styles.ts'
 import { gameOneAction, gameOneState } from '@/pages/GameOne/store'
+import {StreamerDiv} from "@/pages/GameOne/components/Awards/StreamerDiv.tsx";
 
 export default function Awards() {
   const { awards, currentAwardId } = useSnapshot(gameOneState)
@@ -19,19 +20,21 @@ export default function Awards() {
                 return rtn
               }, { total: 0, remain: 0 })
               return (
-                <div
-                  key={award.id}
-                  {...stylex.props(styles.item(award.id === currentAwardId))}
-                  onClick={() => gameOneAction.changeAward(award.id)}
-                >
-                  <div {...stylex.props(styles.image(award.prize[0].image))}></div>
-                  <div {...stylex.props(styles.content)}>
-                    <div {...stylex.props(styles.title)}>
-                      <span>{award.title}</span>
-                      <span {...stylex.props(styles.count)}>{`${remain}/${total}`}</span>
-                    </div>
-                    <div {...stylex.props(styles.prize)}>{award.prize.map(item => item.title).join('、')}</div>
-                  </div>
+                <div key={award.id}>
+                  <StreamerDiv active={award.id === currentAwardId} content={
+                    <div
+                      {...stylex.props(styles.item(award.id === currentAwardId))}
+                      onClick={() => gameOneAction.changeAward(award.id)}
+                    >
+                      <div {...stylex.props(styles.image(award.prize[0].image))}></div>
+                      <div {...stylex.props(styles.content)}>
+                        <div {...stylex.props(styles.title)}>
+                          <span>{award.title}</span>
+                          <span {...stylex.props(styles.count)}>{`${remain}/${total}`}</span>
+                        </div>
+                        <div {...stylex.props(styles.prize)}>{award.prize.map(item => item.title).join('、')}</div>
+                      </div>
+                    </div>}/>
                 </div>
               )
             })
