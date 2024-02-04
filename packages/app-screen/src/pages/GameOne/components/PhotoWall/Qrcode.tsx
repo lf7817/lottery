6a4153ebf90d74feb4d96d020abbb780dbc13d2f
lf7817 <535536456@qq.com>
2024-02-04 20:02:01 +0800

    feat(screen): 修改配置

--- packages/app-screen/src/pages/GameOne/components/PhotoWall/Qrcode.tsx
+++ packages/app-screen/src/pages/GameOne/components/PhotoWall/Qrcode.tsx
@@ -42,7 +42,7 @@ export default function Qrcode() {
   const ref = useRef<Group>(null)
   const { qrcode } = useSnapshot(gameOneState)
   console.log(qrcode)
-  const url = `http://192.168.106.79:4000/sign?activityId=${qrcode}`
+  const url = `/sign?activityId=${qrcode}`
   useRequest(() => fetchUserList(qrcode), { refreshDeps: [qrcode], pollingInterval: 1000 })
 
   useLayoutEffect(() => {
--- packages/app-screen/vite.config.ts
+++ packages/app-screen/vite.config.ts
@@ -4,28 +4,30 @@ import react from '@vitejs/plugin-react-swc'
 import { stylexPlugin } from 'vite-plugin-stylex-dev'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  server: {
-    cors: true,
-    host: '127.0.0.1',
-    port: 4173,
-    proxy: {
-      '/lottery-api': {
-        target: 'http://localhost:4000/lottery-api',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/lottery-api/, ''),
+export default defineConfig(() => {
+  return {
+    base: './',
+    server: {
+      cors: true,
+      host: '127.0.0.1',
+      port: 4173,
+      proxy: {
+        '/lottery-api': {
+          target: 'http://localhost:4000/lottery-api',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/lottery-api/, ''),
+        },
       },
     },
-  },
-  plugins: [react(), stylexPlugin()],
-  resolve: {
-    alias: {
-      '@': pathResolve('src'),
-      // 兼容 css-loader 加载path alias格式 https://github.com/webpack-contrib/css-loader#url
-      '~@': pathResolve('src'),
+    plugins: [react(), stylexPlugin()],
+    resolve: {
+      alias: {
+        '@': pathResolve('src'),
+        // 兼容 css-loader 加载path alias格式 https://github.com/webpack-contrib/css-loader#url
+        '~@': pathResolve('src'),
+      },
     },
-  },
+  }
 })
 
 function pathResolve(...args: string[]) {
