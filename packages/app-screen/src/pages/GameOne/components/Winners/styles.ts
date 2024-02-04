a2e496ec80f75c70ec50f993b2c55d2ea29f6a3a
buran123 <15195860740@163.com>
2024-02-04 13:24:43 +0800

    feat:  颁奖页面设计

---
+++ packages/app-screen/src/pages/GameOne/components/Winners/styles.ts
@@ -0,0 +1,102 @@
+import * as stylex from '@stylexjs/stylex'
+
+export default stylex.create({
+  wrapper: {
+    display:'flex',
+  },
+  left:(url:string)=>({
+    width:'498px',
+    height:'943px',
+    padding:'75px 22px',
+    boxSizing:'border-box',
+    backgroundImage:`url(${url})`,
+    backgroundSize:'100% 100%',
+    backgroundRepeat:'no-repeat'
+  }),
+  right:(url:string,id:string)=>({
+    width:'1131px',
+    height:'943px',
+    marginLeft:'36px',
+    boxSizing:'border-box',
+    backgroundImage:`url(${url})`,
+    backgroundSize:'100% 100%',
+    backgroundRepeat:'no-repeat',
+    display:'flex',
+    justifyContent:'center',
+    flexWrap:'wrap',
+    alignItems:'center',
+    padding:id==='4'?'200px 100px':'300px 0'
+  }),
+  title:{
+    display:'flex',
+    justifyContent:'space-around',
+  },
+  item:(current:string,id:string)=>({
+    fontSize:current===id?'36px':'21px',
+    fontWeight:current===id?'bold':'normal',
+    color:'rgba(252, 230, 184, 1)',
+    cursor:'pointer',
+    lineHeight:'52px'
+  }),
+  pic:{
+    display:'flex',
+    justifyContent:'space-around',
+    boxSizing:'border-box',
+    margin:'90px 0'
+  },
+  btn:(url:string)=>({
+    width:50,
+    height:50,
+    marginTop:125,
+    cursor:'pointer',
+    backgroundImage:`url(${url})`,
+    backgroundSize:'100% 100%',
+    backgroundRepeat:'no-repeat',
+    ':active': {
+      transform: 'scale(0.9)',
+      boxShadow: '3px 2px 22px 1px rgb(0 0 0 / 24%)',
+    },
+  }),
+  xian:(url:string)=>({
+    width:'273px',
+    height:9,
+    backgroundImage:`url(${url})`,
+    backgroundSize:'100% 100%',
+    backgroundRepeat:'no-repeat',
+    margin:'0 auto'
+  }),
+  name:{
+    width:'100%',
+    textAlign:'center',
+    fontSize:'24px',
+    margin:'35px 0'
+  },
+  img:(url:string)=>({
+    width:'300px',
+    height:'300px',
+    backgroundImage:`url(${url})`,
+    backgroundSize:'100% 100%',
+    backgroundRepeat:'no-repeat',
+  }),
+  people:(currentPrize:string)=>({
+    width:currentPrize==='4'?'120px':'200px',
+    height:currentPrize==='4'?'300px':'500px',
+    margin:'0 20px'
+  }),
+  headimgurl:(url:string,currentPrize:string)=>({
+    width:currentPrize==='4'?'120px':'200px',
+    height:currentPrize==='4'?'120px':'200px',
+    borderRadius:'50%',
+    backgroundImage:`url(${url})`,
+    backgroundSize:'100% 100%',
+    backgroundRepeat:'no-repeat',
+  }),
+  username:(currentPrize:string)=>({
+    width:'100%',
+    textShadow: 'rgba(252, 230, 184, 1) 1px 0 10px',
+    fontSize:currentPrize==='4'?'24px':'36px',
+    color:'rgba(252, 230, 184, 1)',
+    textAlign:'center',
+    margin:'30px 0'
+  })
+})
