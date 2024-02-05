4f218d2385eb7ba7a15744968a92ad40143e691e
lf7817 <535536456@qq.com>
2024-02-05 08:34:45 +0800

    feat(screen): 修复二维码路径

--- packages/app-screen/src/pages/GameOne/components/PhotoWall/Qrcode.tsx
+++ packages/app-screen/src/pages/GameOne/components/PhotoWall/Qrcode.tsx
@@ -42,7 +42,7 @@ export default function Qrcode() {
   const ref = useRef<Group>(null)
   const { qrcode } = useSnapshot(gameOneState)
   console.log(qrcode)
-  const url = `/sign?activityId=${qrcode}`
+  const url = `/lottery/sign?activityId=${qrcode}`
   useRequest(() => fetchUserList(qrcode), { refreshDeps: [qrcode], pollingInterval: 1000 })
 
   useLayoutEffect(() => {
